49f117158e33a5fe3fb5ac6dcc01603d09e5fe74
Nemanja Avramović <avramyu@gmail.com>
2018-03-01 21:48:24 +0100

    fix(site): Added documentation link to front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/avramovic/breadly/wiki">Documentation</a>
+                    <a href="https://github.com/avramovic/breadly">GitHub</a>
                 </div>
             </div>
         </div>
