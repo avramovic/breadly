6f54f871bdc1e08286a74e9202c2287cfabd14c7
Nemanja Avramović <avramyu@gmail.com>
2018-03-06 22:39:23 +0100

    fix(admin): Prevent unauthorized users from changing their own role

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Events\BreadDataUpdated;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
+
+class AdminUserController extends \TCG\Voyager\Http\Controllers\VoyagerBreadController
+{
+    use BreadRelationshipParser;
+
+    // POST BR(E)AD
+    public function update(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Compatibility with Model binding.
+        $id = $id instanceof Model ? $id->{$id->getKeyName()} : $id;
+
+        $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
+
+        // Check permission
+        $this->authorize('edit', $data);
+
+        if(! Auth::user()->hasPermission('edit_roles')) {
+            unset($request['role_id']);
+        }
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->editRows);
+
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->ajax()) {
+            $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+
+            event(new BreadDataUpdated($dataType, $data));
+
+            return back()
+                ->with([
+                    'message'    => __('voyager.generic.successfully_updated')." {$dataType->display_name_singular}",
+                    'alert-type' => 'success',
+                ]);
+        }
+    }
+}
