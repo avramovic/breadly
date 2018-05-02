d6103bb675dbfe6bcfaf9fe06886dddbdd81375a
Nemanja Avramović <avramyu@gmail.com>
2018-05-02 12:38:31 +0200

    fix(core): Convert all emails in HTTP requests to lowercase

---
+++ app/Http/Middleware/ConvertEmailsToLowerCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ConvertEmailsToLowerCase
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (isset($request->email)) {
+            $data          = $request->all();
+            $data['email'] = strtolower($data['email']);
+            $request->replace($data);
+        }
+
+        return $next($request);
+    }
+}
