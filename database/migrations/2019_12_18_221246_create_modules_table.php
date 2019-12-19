ab5bda26842dffe80791261a752d7b9b1ec6e80a
Nemanja Avramović <avramyu@gmail.com>
2019-12-19 09:57:22 +0100

    feat(modules): Added database module driver

---
+++ database/migrations/2019_12_18_221246_create_modules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('basename')->index();
+            $table->string('crc32');
+            $table->string('name');
+            $table->string('slug')->index();
+            $table->string('version')->default('1.0');
+            $table->text('description')->nullable();
+            $table->integer('order')->default(9001);
+            $table->boolean('enabled')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modules');
+    }
+}
