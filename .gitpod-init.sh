0d9ec90c880e76190cdbc49a03e266989366c5f1
avramovic <avramyu@gmail.com>
2020-11-20 21:55:38 +0000

    added gitpod welcome msg

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -1,7 +1,30 @@
+## :::::::::  :::::::::  ::::::::::     :::     :::::::::  :::     :::   ::: 
+## :+:    :+: :+:    :+: :+:          :+: :+:   :+:    :+: :+:     :+:   :+: 
+## +:+    +:+ +:+    +:+ +:+         +:+   +:+  +:+    +:+ +:+      +:+ +:+  
+## +#++:++#+  +#++:++#:  +#++:++#   +#++:++#++: +#+    +:+ +#+       +#++:   
+## +#+    +#+ +#+    +#+ +#+        +#+     +#+ +#+    +#+ +#+        +#+    
+## #+#    #+# #+#    #+# #+#        #+#     #+# #+#    #+# #+#        #+#    
+## #########  ###    ### ########## ###     ### #########  ########## ### 
+##
+## Welcome to your own personal Breadly gitpod instance
+##
+## To start the development server type the following command in the terminal:  
+##
+## php artisan serve
+##
+## The first time you do this it will ask you to make port 8000 public. You should 
+## do this in order to make your instance publicly available.
+##
+## Your public URL will be:
+##
+## http://localhost
+##
 mysql -u root -e "create database breadly"
 cp .env.example .env
 sed -i "s|APP_URL=http://localhost|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+sed -i "s|http://localhost|${GITPOD_WORKSPACE_URL}|g" .gitpod-init.sh
+sed -i "s|=https://|https://8000-|g" .gitpod-init.sh
 composer install
 php artisan key:generate
 php artisan migrate --seed
